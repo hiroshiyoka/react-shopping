9bde6b45973de96a98b8b5cb26e37f41228b08c8
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-07-26 21:21:38 +0800

    add a discount

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -53,14 +53,14 @@ function ProductInfo(props) {
       <p className="cate">{category}</p>
       <h1 className="title">{name}</h1>
       <p className="price">IDR 1.250.000</p>
-      <CheckDiscount isDiscount={isDiscount} discount={45} />
+      <CheckDiscount isDiscount={isDiscount} discount={35} />
       <p className="info">
       The Sk8-Hi, Vans legendary lace-up high top inspired by the classic Old Skool, has a durable canvas and suede upper, a supportive and padded ankle, and Vans vulcanized signature Waffle Outsole.
       </p>
       <ul>
         {listBenefits}
       </ul>
-      <a onClick={(e) => addCart(name, e)} href="//#endregion">Add to Cart</a>
+      <a onClick={(e) => addCart(name, e)} href="#">Add to Cart</a>
     </div>
     </div>
   );
